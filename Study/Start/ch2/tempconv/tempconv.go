c4c3541198b4d7470a2d60d72a14b12d1805cd45
KakioJ <1589610753@qq.com>
2025-03-11 16:24:28 +0800

    增加Kelvin类型

--- Study/Start/ch2/tempconv/tempconv.go
+++ Study/Start/ch2/tempconv/tempconv.go
@@ -4,11 +4,13 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
+	AbsoluteZeroK Kelvin = -273.15
 )
 
 // func CToF(c Celsius) Fahrenheit {
@@ -21,6 +23,7 @@ const (
 
 func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 
+func (k Kelvin) String() string { return fmt.Sprintf("%g°K", k) }
 
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 
